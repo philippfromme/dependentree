ed4efa8c5b9e059d8a82465949771aef818483ee
Philipp <philippfromme@outlook.com>
2022-11-25 21:32:41 +0100

    fix: downgrade to React v16

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import { render } from "react-dom";
 
 import App from "./components/App";
 
 import "./index.scss";
 
-const rootElement = document.getElementById("app");
-
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+render(<App />, document.getElementById("app"));
